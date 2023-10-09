eb80e8fba7e24713f23ef75416f2a46eb75d1daa
Daniel Brian <db9755949@gmail.com>
2023-10-09 23:16:06 +0300

    Jetpack Compose

---
+++ app/src/main/java/daniel/brian/jetpackcompose/MainActivity.kt
@@ -0,0 +1,144 @@
+package daniel.brian.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import daniel.brian.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+
+    // Instead of calling this functionality inside the Onboarding screen we hoisted it at the common ancestor
+    @Composable
+    fun MyApp(modifier: Modifier = Modifier) {
+        // by - helps us to avoid using .value - it is a delegate property
+        var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+        Surface(modifier) {
+            if (shouldShowOnboarding) {
+                OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+            } else {
+                Greetings()
+            }
+        }
+    }
+
+    @Composable
+    private fun Greetings(
+        modifier: Modifier = Modifier,
+        // Using a listOf usually takes small a number of items with it.
+        // names: List<String> = listOf("Daniel", "Brian"),
+        names: List<String> = List(1000) { "Daniel" },
+    ) {
+        LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+            items(items = names) { name ->
+                Greeting(name = name)
+            }
+        }
+    }
+
+    @Preview(showBackground = true, widthDp = 320)
+    @Composable
+    private fun GreetingsPreview() {
+        JetpackComposeTheme {
+            Greetings()
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        val expanded = remember { mutableStateOf(false) }
+        val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+        Surface(
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        ) {
+            Row(modifier = Modifier.padding(24.dp)) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(extraPadding),
+                ) {
+                    Text(text = "Hello,")
+                    Text(text = name)
+                }
+                ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                    Text(if (expanded.value) "Show less" else "Show more")
+                }
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        JetpackComposeTheme {
+            MyApp()
+        }
+    }
+
+    @Composable
+    fun OnboardingScreen(
+        modifier: Modifier = Modifier,
+        onContinueClicked: () -> Unit,
+    ) {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked,
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+
+    @Preview(showBackground = true, widthDp = 320, heightDp = 320)
+    @Composable
+    fun OnboardingPreview() {
+        JetpackComposeTheme {
+            OnboardingScreen(onContinueClicked = {}) // Do nothing on click
+        }
+    }
+
+    @Preview
+    @Composable
+    fun MyAppPreview() {
+        JetpackComposeTheme {
+            MyApp(Modifier.fillMaxSize())
+        }
+    }
+}
