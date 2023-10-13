c48a657a860bc3e2ed339e58c2b101c3ba1e2269
Daniel Brian <db9755949@gmail.com>
2023-10-13 16:51:45 +0300

    Compose Layouts

--- ComposeLayouts/app/src/main/java/daniel/brian/composelayouts/MainActivity.kt
+++ ComposeLayouts/app/src/main/java/daniel/brian/composelayouts/MainActivity.kt
@@ -3,9 +3,18 @@ package daniel.brian.composelayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,7 +25,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,13 +41,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeLayoutsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    SearchBar()
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
+        }
+    }
+}
+
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background,
+    ) {
+        Column(
+            modifier = modifier,
+        ) {
+            SearchBar()
+            AlignYourBodyElement(
+                text = R.string.Inversions,
+                drawable = R.drawable.inversions,
+            )
         }
     }
 }
@@ -44,26 +72,78 @@ class MainActivity : ComponentActivity() {
 fun SearchBar(
     modifier: Modifier = Modifier,
 ) {
-    TextField(
-        value = "",
-        onValueChange = {},
-        leadingIcon = {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = null,
-            )
-        },
-        colors = TextFieldDefaults.textFieldColors(
-            unfocusedIndicatorColor = MaterialTheme.colorScheme.surface,
-            focusedIndicatorColor = MaterialTheme.colorScheme.surface,
-        ),
-        placeholder = {
-            Text(stringResource(R.string.search))
-        },
-        modifier = modifier
-            .fillMaxWidth()
-            .heightIn(min = 6.dp),
-    )
+    Row(
+        modifier = modifier.padding(15.dp),
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                unfocusedIndicatorColor = MaterialTheme.colorScheme.surface,
+                focusedIndicatorColor = MaterialTheme.colorScheme.surface,
+            ),
+            placeholder = {
+                Text(stringResource(R.string.search))
+            },
+            modifier = modifier
+                .fillMaxWidth()
+                .heightIn(min = 56.dp),
+        )
+    }
+}
+
+@Composable
+fun AlignYourBodyElement(
+    // making images and text dynamic
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(15.dp),
+    ) {
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop,
+        )
+        Text(
+            stringResource(text),
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlignBodyElementPreview() {
+    ComposeLayoutsTheme {
+        AlignYourBodyElement(
+            // in the preview we are passing hard-coded data
+            text = R.string.Inversions,
+            drawable = R.drawable.inversions,
+            modifier = Modifier.padding(8.dp),
+        )
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    ComposeLayoutsTheme {
+        MyApp()
+    }
 }
 
 @Preview(showBackground = true)
